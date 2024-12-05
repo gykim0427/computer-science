c034c89be140ae642c430c9a32ed2851d9d4681d
gykim <gykim@hnworks.co.kr>
2024-12-05 09:59:50 +0900

    baekjoon 10826

---
+++ src/main/baekjoon/Bj10826.java
@@ -0,0 +1,24 @@
+package main.baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Bj10826 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		BigInteger[] bi = new BigInteger[n+2];
+		
+		bi[0] = new BigInteger("0");
+		bi[1] = new BigInteger("1");
+		
+		for (int i=2; i<bi.length; i++) {
+			bi[i] = bi[i-1].add(bi[i-2]);
+		}
+		
+		System.out.println(bi[n]);
+	}
+
+}
