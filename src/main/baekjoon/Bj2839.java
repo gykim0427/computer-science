75bcd98a79edb8c7cc22ee9853dfae2fb2be7ede
gykim <gykim0427@naver.com>
2024-12-06 16:00:21 +0900

    bj2839

--- src/main/baekjoon/Bj2839.java
+++ src/main/baekjoon/Bj2839.java
@@ -6,9 +6,27 @@ public class Bj2839 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int n = sc.nextInt();
+		
 		int answer = 0;
+		
+		if (n==3) {
+            answer = 1;
+        } if (n==4 || n==7) {
+			answer = -1;
+		} if (n%5==0) {
+			answer = n/5;
+		} if (n%5==1) {
+            answer = n/5 + 1;
+        } if (n!=7 && n%5==2) {
+            answer = n/5 + 2;
+        } if (n>5 && n%5==3) {
+            answer = n/5 + 1;
+        } if (n>5 && n%5==4) {
+            answer = n/5 + 2;
+        }
+        
+		System.out.println(answer);
 	}
 
 }
