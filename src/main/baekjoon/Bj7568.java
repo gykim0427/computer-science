a9dff8dc319dceabc90b4ae7b1a1908cbe19d17d
gykim <gykim0427@naver.com>
2024-12-16 16:08:09 +0900

    bj7568

--- src/main/baekjoon/Bj7568.java
+++ src/main/baekjoon/Bj7568.java
@@ -8,10 +8,26 @@ public class Bj7568 {
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
+        int[][] arr = new int[n][2];
         
         for (int i=0; i<n; i++) {
+        	int weight = sc.nextInt();
+        	int height = sc.nextInt();
         	
+        	arr[i][0] = weight;
+        	arr[i][1] = height;
         }
-}
-
+        
+        for (int i=0; i<n; i++) {
+        	int rank = 1;
+        	
+        	for (int j=0; j<n; j++) {
+        		if ((arr[i][0]<arr[j][0]) && (arr[i][1]<arr[j][1])) {
+        			rank++;
+        		}
+        	}	
+        	
+        	System.out.print(rank + " ");
+        }
+	}
 }
