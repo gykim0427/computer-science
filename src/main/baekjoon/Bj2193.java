9049458a24466c60672bfcc1b8c866d634b4d17b
gykim <gykim0427@naver.com>
2024-12-19 17:25:57 +0900

    bj2193 final

--- src/main/baekjoon/Bj2193.java
+++ src/main/baekjoon/Bj2193.java
@@ -1,20 +1,24 @@
 package main.baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Bj2193 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt();
-		int answer = 0;
+		int n = Integer.parseInt(br.readLine());
+		long[] dp = new long[n+1];
 		
-		if (n==1 || n==2) {
-			answer = 1;
-		} 
+		dp[0] = 0;
+		dp[1] = 1;
+				
+		for (int i=2; i<=n; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
 		
-		
-		System.out.println(answer);
+		System.out.println(dp[n]);
 	}
 }
