0a3fc81f2ba86088ea810309f5fffb538a66ff07
gy kim <gykim0427>
2024-12-23 21:52:57 +0900

    bj1932 second commit

--- src/main/baekjoon/Bj1932.java
+++ src/main/baekjoon/Bj1932.java
@@ -1,11 +1,19 @@
 package main.baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Bj1932 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][];
+		
+		for (int i=0; i<n; i++) {
+			
+		}
 	}
-
 }
