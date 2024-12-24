be6d27fb4ec2ccfff2567239226c3fde06dfac22
gy kim <gykim0427@naver.com>
2024-12-24 23:01:43 +0900

    bj1932 third

--- src/main/baekjoon/Bj1932.java
+++ src/main/baekjoon/Bj1932.java
@@ -3,6 +3,7 @@ package main.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Bj1932 {
 
@@ -13,7 +14,21 @@ public class Bj1932 {
 		int[][] arr = new int[n][];
 		
 		for (int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
+			arr[i] = new int[i + 1];
+			
+			for (int j=0; j<=i; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
+		
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<i; j++) {
+				
+			}
+		}
+		
+		System.out.println();
 	}
 }
