259f23c14f5199de3b5a084d6dc4c0f35475c58b
gykim <gykim0427@naver.com>
2024-12-26 15:41:08 +0900

    bj1932 final

--- src/main/baekjoon/Bj1932.java
+++ src/main/baekjoon/Bj1932.java
@@ -23,12 +23,12 @@ public class Bj1932 {
 			}
 		}
 		
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<i; j++) {
-				
-			}
+		for (int i=n-1; i>0; i--) {
+		    for (int j=0; j<i; j++) {
+		        arr[i-1][j] += Math.max(arr[i][j], arr[i][j+1]);
+		    }
 		}
 		
-		System.out.println();
+		System.out.println(arr[0][0]);
 	}
 }
