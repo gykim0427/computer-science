b7fadc8977a4cedc998ce43cc012569e6cd146f8
gy kim <gykim0427@naver.com>
2024-12-28 23:16:58 +0900

    bj10828 second

--- src/main/baekjoon/Bj10828.java
+++ src/main/baekjoon/Bj10828.java
@@ -1,13 +1,21 @@
 package main.baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Bj10828 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		Stack<Integer>	stack = new Stack<>();
 		
-		int n = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i=0; i<n; i++) {
+			
+		}
 	}
-
+	
 }
