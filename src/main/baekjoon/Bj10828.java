a4343fc9bdee9af2a223e4e94d9e042c02e26c2b
gy kim <gykim0427@naver.com>
2024-12-29 21:02:16 +0900

    bj 10828

--- src/main/baekjoon/Bj10828.java
+++ src/main/baekjoon/Bj10828.java
@@ -7,14 +7,45 @@ import java.util.Stack;
 public class Bj10828 {
 
 	public static void main(String[] args) throws Exception {
-		Stack<Integer>	stack = new Stack<>();
+		Stack<Integer> stack = new Stack<>();
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
 		
 		for (int i=0; i<n; i++) {
+			String[] strArr = br.readLine().split(" ");
 			
+			switch (strArr[0]) {
+				case "push" :
+					int x = Integer.parseInt(strArr[1]);
+					stack.push(x);
+					break;
+				case "pop" :
+					if (!stack.isEmpty()) {
+                        System.out.println(stack.pop());
+					} else {
+						System.out.println("-1");
+					}
+					break;
+				case "size" :
+					System.out.println(stack.size());
+					break;
+				case "empty" :
+					if (!stack.isEmpty()) {
+                        System.out.println("0");
+					} else {
+						System.out.println("1");
+					}
+        			break;
+				case "top" :
+					if (!stack.isEmpty()) {
+                        System.out.println(stack.peek());
+					} else {
+						System.out.println("-1");
+					}
+        			break;
+			}
 		}
 	}
 	
