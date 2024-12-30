a76bd18e39b2abc8ee0c5b937348e75a2852c553
gykim <gykim0427@naver.com>
2024-12-30 12:49:33 +0900

    bj10845 first

---
+++ src/main/baekjoon/Bj10845.java
@@ -0,0 +1,13 @@
+package main.baekjoon;
+
+import java.util.Scanner;
+
+public class Bj10845 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+	}
+
+}
