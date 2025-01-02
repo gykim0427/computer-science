f1b95700b515a87426762aa5c14d99d530b7051d
gykim <gykim0427@naver.com>
2025-01-02 09:22:28 +0900

    bj11047 first

---
+++ src/main/baekjoon/Bj11047.java
@@ -0,0 +1,18 @@
+package main.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj11047 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i=0; i<n; i++) {
+			
+		}
+	}
+
+}
