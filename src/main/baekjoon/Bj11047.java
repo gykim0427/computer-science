a0b312680b08382f3329576be80ad48dbd493eb8
gykim <gykim0427@naver.com>
2025-01-03 12:49:05 +0900

    bj11047 second

--- src/main/baekjoon/Bj11047.java
+++ src/main/baekjoon/Bj11047.java
@@ -2,16 +2,26 @@ package main.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Bj11047 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
+		String[] str = br.readLine().split(" ");
+		
+		int n = Integer.parseInt(str[0]);
+		int k = Integer.parseInt(str[1]);
+		
+		ArrayList<Integer> arrList = new ArrayList<>();
 		
 		for (int i=0; i<n; i++) {
+			int unit = Integer.parseInt(br.readLine());
 			
+			if (unit) {
+				
+			}
 		}
 	}
 
