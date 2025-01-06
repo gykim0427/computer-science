740b5b9f10b83c0dd6959f436a6063646afb934d
gykim <gykim0427@naver.com>
2025-01-06 16:46:04 +0900

    bj1026 start

---
+++ src/main/baekjoon/Bj1026.java
@@ -0,0 +1,29 @@
+package main.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Bj1026 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		ArrayList<Integer> arrListA = new ArrayList<>();
+		ArrayList<Integer> arrListB = new ArrayList<>();
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+				
+		for (int i=0; i<n; i++) {
+			a
+			b
+			
+			arrListA.add(a);
+			arrListB.add(b);
+		}
+	}
+
+}
