bf5906c652dcc5d70cf4b545685a008c5cc204bf
gykim <gykim0427@naver.com>
2025-01-08 08:55:56 +0900

    bj1316

---
+++ src/main/baekjoon/Bj1316.java
@@ -0,0 +1,23 @@
+package main.baekjoon;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class Bj1316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int cnt = 0;
+		
+		for (int i=0; i<n; i++) {
+			String str = sc.next();
+			
+			if (str.) {
+				
+			}
+		}
+	}
+
+}
