6492c907c9fce1d882e4f9d0ee23897775781ac5
gykim <gykim0427@naver.com>
2025-01-10 14:49:32 +0900

    bj1316 final

--- src/main/baekjoon/Bj1316.java
+++ src/main/baekjoon/Bj1316.java
@@ -5,32 +5,46 @@ import java.io.InputStreamReader;
 
 public class Bj1316 {
 
-	public static void main(String[] args) {
+	/**
+	 * @param args
+	 * @throws Exception
+	 * @Needs review
+	 */
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
 		int cnt = 0;
-		int prev = 0;
-		
-		String str = br.readLine();
-		
-		boolean[] bl = new boolean[26];
-		
-		for (int i=0; i<str.length(); i++) {
-			int now = str.charAt(i);
-			
-			if (prev != now) {
-				if (!bl[now - 'a']) {
-					
-				}
-			}
-		}
 		
 		for (int i=0; i<n; i++) {
-			if (str.) {
+			String str = br.readLine();
+			int prev = 0;
+			boolean[] blArr = new boolean[26];
+			boolean bl = false;
+			
+			for (int j=0; j<str.length(); j++) {
+				int now = str.charAt(j);
 				
+				System.out.println("prev : " + prev + " || now : "+ now + " || blArr : " + blArr[now - 'a']);
+				
+				if (prev != now) {
+					if (!blArr[now - 'a']) {
+						blArr[now - 'a'] = true;
+						prev = now;
+						bl = true;
+					} else {
+						bl = false;
+						break;
+					}
+				}
+			}
+			
+			if (bl) {
+				cnt++;
 			}
 		}
+		System.out.println(cnt);
+		
 	}
 
 }
