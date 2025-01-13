819bb8b0c0b6e0b0eb6821e6b9f66c4bfbcb9c0e
gykim <gykim0427@naver.com>
2025-01-13 17:04:38 +0900

    bj2941 second

--- src/main/baekjoon/Bj2941.java
+++ src/main/baekjoon/Bj2941.java
@@ -9,8 +9,22 @@ public class Bj2941 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String str = br.readLine();
+		String croStr = "c=,c-,dz=,d-,lj,nj,s=,z=";
+
+		String[] croArr = croStr.split(",");
 		
+		int cnt = 0;
 		
+		// **
+		for (int i=0; i<croArr.length;i++) {
+			for (int j=0; j<str.length();j++) {
+				if (str.contains(croArr[i])) {
+					cnt++;
+				}
+			}
+		}
+		
+		System.out.println(cnt);
 	}
 
 }
