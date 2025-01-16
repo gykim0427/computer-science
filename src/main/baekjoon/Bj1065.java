520e5a6cffb32de0a9c80f4b6b9030c1edbcc08c
gykim <gykim0427@naver.com>
2025-01-16 13:00:30 +0900

    bj1065 second

--- src/main/baekjoon/Bj1065.java
+++ src/main/baekjoon/Bj1065.java
@@ -1,13 +1,29 @@
 package main.baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Bj1065 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt();
+		String str = br.readLine();
+		
+		int n = Integer.parseInt(str);
+		int cnt = 0;
+		
+		for (int i=0; i<str.length(); i++) {
+			str.charAt(i);
+		}
+		
+		for (int i=1; i<=n; i++) {
+			if (i>0 && i<100) {
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
 	}
-
+	
 }
