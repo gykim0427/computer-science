0a6e0deacd1a0cbb30484512850065b4a7add4f7
gy kim <gykim0427@naver.com>
2025-01-18 13:00:47 +0900

    bj1018 first

---
+++ src/main/baekjoon/Bj1018.java
@@ -0,0 +1,18 @@
+package main.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj1018 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		String[] strArr = new String[2];
+		
+		strArr = str.split(" ");	
+		
+	}
+
+}
