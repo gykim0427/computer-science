32410f323088358fafd6b73b3cc82664a86f12aa
gykim <gykim0427@naver.com>
2025-01-24 15:04:53 +0900

    bj11650

---
+++ src/main/baekjoon/Bj11650.java
@@ -0,0 +1,14 @@
+package main.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj11650 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+	}
+
+}
