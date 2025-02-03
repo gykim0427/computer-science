d82a49e486151588c9cfcef79499bec5eb50d4ea
gy kim <gykim0427@naver.com>
2025-02-03 22:16:23 +0900

    bj11650 second

--- src/main/baekjoon/Bj11650.java
+++ src/main/baekjoon/Bj11650.java
@@ -2,6 +2,8 @@ package main.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Bj11650 {
 
@@ -9,6 +11,20 @@ public class Bj11650 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		for (int i=0; i<n; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 
 }
