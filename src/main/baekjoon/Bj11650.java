abc19243864538bb8ba0fa0f190d06826418cdb9
gykim <gykim0427@naver.com>
2025-02-04 15:33:15 +0900

    bj11650 final

--- src/main/baekjoon/Bj11650.java
+++ src/main/baekjoon/Bj11650.java
@@ -3,6 +3,7 @@ package main.baekjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Bj11650 {
@@ -12,18 +13,20 @@ public class Bj11650 {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		int[] arr = new int[n];
+		int[][] arr = new int[n][2];
 		
 		for (int i=0; i<n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr);
+		Arrays.sort(arr, Comparator.comparingInt((int[] a) -> a[0])
+				.thenComparingInt(a -> a[1]));
 		
 		for (int i=0; i<n; i++) {
-			System.out.println(arr[i]);
+			System.out.println(arr[i][0] + " " + arr[i][1]);
 		}
 	}
 
