35fcb8af06ff8fc1da8ee258f4e24817d17cf8f6
gy kim <gykim0427@naver.com>
2025-02-06 21:59:16 +0900

    bj10814 second

--- src/main/baekjoon/Bj10814.java
+++ src/main/baekjoon/Bj10814.java
@@ -2,6 +2,8 @@ package main.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 import sun.util.locale.StringTokenIterator;
@@ -13,14 +15,21 @@ public class Bj10814 {
 
 		int n = Integer.parseInt(br.readLine());
 		
-		String[][] arr = new String[n][];
+		String[][] arr = new String[n][2];
 		
 		for (int i=0; i<n; i++) {
-			StringTokenizer st = new StringTokenizer(arr[i][], " ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			arr[i][0] = st.nextToken();
 			arr[i][1] = st.nextToken();
 		}
+		
+		Arrays.sort(arr, new Comparator<String[]>() {
+			@Override
+			public int compare() {
+				
+			}
+		});
 		
 		for (int i=0; i<n; i++) {
 			System.out.println(arr[i][0] + " " +arr[i][1]);
