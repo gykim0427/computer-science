02f632a24746b4239c9bbacd78322ce88ccfb6fc
gy kim <gykim0427@naver.com>
2025-02-22 13:31:26 +0900

    bj6603 final

--- src/main/algorithm/baekjoon/Bj6603.java
+++ src/main/algorithm/baekjoon/Bj6603.java
@@ -4,28 +4,63 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// hard
 public class Bj6603 {
-
+	static int n; 
+	static int[] arr;
+	static int[] result;
+	
+	static boolean[] visit;
+	
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		while (true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			int n = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
 			
 			if (n==0) {
 				break;
 			}
 			
-			int[] arr = new int[n];
+			arr = new int[n];
 			
-			for (int i : arr) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
+            for (int i=0; i<n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
 			
-			System.out.println();
+			result = new int[6];
+			visit = new boolean[n]; 
+			
+			back(0, 0);
+			
+			sb.append("\n");
 		}
+		System.out.println(sb);	
 	}
 	
+    public static void back(int start, int depth) {
+        if (depth == 6) {
+            for (int val : result) {
+                sb.append(val).append(" ");
+            }
+            
+            sb.append("\n");
+            return;
+        }
+
+        for (int i=start; i<n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                result[depth] = arr[i];
+                
+                back(i, depth + 1);
+                
+                visit[i] = false;
+            }
+        }
+    }
 }
