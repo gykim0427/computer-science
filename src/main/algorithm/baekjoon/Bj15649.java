397a24965bc8ca6e2155df5325fccec71da5f66e
gy kim <gykim0427@naver.com>
2025-02-23 21:26:32 +0900

    bj15649

---
+++ src/main/algorithm/baekjoon/Bj15649.java
@@ -0,0 +1,15 @@
+package main.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bj15649 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer();
+	}
+
+}
