ee94756ac39a40786b019cf966290b8e4c4ccf85
gy kim <gykim0427@naver.com>
2025-02-24 22:29:13 +0900

    bj15649 second

--- src/main/algorithm/baekjoon/Bj15649.java
+++ src/main/algorithm/baekjoon/Bj15649.java
@@ -6,10 +6,16 @@ import java.util.StringTokenizer;
 
 public class Bj15649 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringTokenizer st = new StringTokenizer();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] nArr = new int[Integer.parseInt(st.nextToken())];
+		
+		for (int i : nArr) {
+			System.out.println(nArr[i]);
+		}
 	}
 
 }
