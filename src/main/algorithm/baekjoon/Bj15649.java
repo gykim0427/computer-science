61e7a06b6167fc3f76470e661f2bcbf50956bdf8
gy kim <gykim0427@naver.com>
2025-02-28 23:12:02 +0900

    bj15649 final

--- src/main/algorithm/baekjoon/Bj15649.java
+++ src/main/algorithm/baekjoon/Bj15649.java
@@ -4,8 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// hard
 public class Bj15649 {
 
+	public static int[] arr;
+	public static boolean[] visit;
+	public static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -13,12 +18,35 @@ public class Bj15649 {
 		
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-			
-		back(0);
+		
+		arr = new int[m];
+		visit = new boolean[n];
+		
+		dfs(n, m, 0);
+		
+		System.out.println(sb);
 	}
 	
-	public static boolean back(int a) {
-		return true;
+	public static void dfs(int N, int M, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val).append(' ');
+			}
+			sb.append('\n');
+			
+			return;
+		}
+ 
+		for (int i = 0; i < N; i++) {
+			if (!visit[i]) {
+				visit[i] = true;
+				arr[depth] = i + 1;
+				
+				dfs(N, M, depth + 1);
+				
+				visit[i] = false;
+			}
+		}
 	}
 
 }
