9a64e380f802313a6a970210224f57917243696a
gy kim <gykim0427@naver.com>
2025-03-01 15:58:34 +0900

    bj1914 first

---
+++ src/main/algorithm/baekjoon/Bj1914.java
@@ -0,0 +1,19 @@
+package main.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class Bj1914 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int ) {
+			back();
+		}
+		
+		System.out.println(n);
+	}
+
+}
