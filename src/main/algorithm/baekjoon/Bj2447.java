2999c6b223c59ed23b519dceb175ba94a9f1b94d
gy kim <gykim0427@naver.com>
2025-03-28 23:07:27 +0900

    bj2447 second

--- src/main/algorithm/baekjoon/Bj2447.java
+++ src/main/algorithm/baekjoon/Bj2447.java
@@ -1,10 +1,18 @@
 package main.algorithm.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Bj2447 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i=0; i<n; i++) {
+			System.out.println("*");
+		}
 	}
 
 }
