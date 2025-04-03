3ece608e0524e27b40b863f61c8bfce0d8dde65b
gy kim <gykim0427@naver.com>
2025-04-03 20:01:07 +0900

    bj2447 fifth

--- src/main/algorithm/baekjoon/Bj2447.java
+++ src/main/algorithm/baekjoon/Bj2447.java
@@ -1,24 +1,30 @@
 package main.algorithm.baekjoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 // hard
 public class Bj2447 {
-	static int[][] arr;
+	static char[][] arr;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		arr = new int[n][n];
+		arr = new char[n][n];
 		
 		recursion(0, 0, n, false);
 		
 		for (int i=0; i<n; i++) {
-			
+			bw.write(arr[i]);
+			bw.write("\n");
 		}
+		bw.flush();
+		bw.close();
 	}
 	
 	static void recursion(int x, int y, int n, boolean check) {
@@ -29,6 +35,24 @@ public class Bj2447 {
 				}
 			}
 			return;
+		}
+		
+		if (n == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+		
+		int size = n/3;
+		int count = 0;
+		for (int i=x; i<x+n; i+=size) {
+			for (int j=y; j<y+n; j+=size) {
+				count++;
+				if (count == 5) {
+					recursion(i, j, size, true);
+				} else {
+					recursion(i, j, size, false);
+				}
+			}
 		}
 	}
 }
