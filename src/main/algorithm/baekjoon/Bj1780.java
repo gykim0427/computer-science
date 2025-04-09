0dba83ef7e52ebb17d88a868d04370db5af6b7be
gykim <gykim0427@naver.com>
2025-04-09 14:29:57 +0900

    bj1780 first commit

---
+++ src/main/algorithm/baekjoon/Bj1780.java
@@ -0,0 +1,24 @@
+package main.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//hard
+//tistory writing 4
+public class Bj1780 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n][n];
+		
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				
+			}
+		}
+	}
+
+}
