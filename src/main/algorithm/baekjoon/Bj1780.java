e155555c80e87221dbca546d28add3d047af2ea2
gykim <gykim0427@naver.com>
2025-04-10 15:50:47 +0900

    bj1780 second commit

--- src/main/algorithm/baekjoon/Bj1780.java
+++ src/main/algorithm/baekjoon/Bj1780.java
@@ -2,6 +2,7 @@ package main.algorithm.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 //hard
 //tistory writing 3
@@ -9,16 +10,29 @@ public class Bj1780 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
 		
 		int[][] arr = new int[n][n];
 		
 		for (int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
 			for (int j=0; j<n; j++) {
-				
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
+		recursion();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+	
+	public static void recursion() {
+		return;
 	}
 
 }
