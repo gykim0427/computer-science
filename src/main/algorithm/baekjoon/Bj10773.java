f284d9ae174b7ad939dcaa73d1fbfce062954114
gykim <gykim0427@naver.com>
2025-04-21 22:05:37 +0900

    bj10773 source assessment

--- src/main/algorithm/baekjoon/Bj10773.java
+++ src/main/algorithm/baekjoon/Bj10773.java
@@ -8,9 +8,10 @@ public class Bj10773 {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] a = new int[n];
         int position = -1;		//연산할 배열의 최종위치
         int sum = 0;
+        
+        int[] a = new int[n];
 
         for (int i=0;i<n;i++) {
             int m = sc.nextInt();
@@ -18,7 +19,7 @@ public class Bj10773 {
             if (m == 0) {
                 position--;
             } else {
-                position++;
+                position++;    
                 a[position] = m;
             }
         }
