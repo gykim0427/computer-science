5a6255e689cdc6ca8de8a47ee87ab6585fe7a1ab
gykim <gykim0427@naver.com>
2025-07-25 08:30:19 +0900

    bj11047 source assessment

--- src/main/algorithm/baekjoon/Bj11047.java
+++ src/main/algorithm/baekjoon/Bj11047.java
@@ -10,7 +10,6 @@ public class Bj11047 {
 		BufferedReader	br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String[] str = br.readLine().split(" ");
-		
 		int n = Integer.parseInt(str[0]);
 		int k = Integer.parseInt(str[1]);
 		
