ed4b5a077ca27b01927230af0f714d342e0087c4
gykim <gykim0427@naver.com>
2025-08-11 08:24:51 +0900

    bj10828 source assseement

--- src/main/algorithm/baekjoon/Bj10828.java
+++ src/main/algorithm/baekjoon/Bj10828.java
@@ -7,9 +7,7 @@ import java.util.Stack;
 public class Bj10828 {
 	public static void main(String[] args) throws Exception {
 		Stack<Integer> stack = new Stack<>();
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int n = Integer.parseInt(br.readLine());
 		
 		for (int i=0; i<n; i++) {
