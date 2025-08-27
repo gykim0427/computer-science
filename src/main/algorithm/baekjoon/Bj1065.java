7044ce152cd5c68d137ff93d07ad4fd1a7078001
gykim <gykim0427@naver.com>
2025-08-28 08:25:31 +0900

    bj1065 source assessment

--- src/main/algorithm/baekjoon/Bj1065.java
+++ src/main/algorithm/baekjoon/Bj1065.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 public class Bj1065 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String str = br.readLine();
 		
 		int n = Integer.parseInt(str);
