c509d5cc62f9e8e041131b771c59e99bf1ec87e3
gykim <gykim0427@naver.com>
2025-09-09 09:34:45 +0900

    bj1018 source assessment

--- src/main/algorithm/baekjoon/Bj1018.java
+++ src/main/algorithm/baekjoon/Bj1018.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 public class Bj1018 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
