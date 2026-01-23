e4a8bd8a597b152bf25d12ca8eeff630db8c6aee
gykim <gykim0427@naver.com>
2026-01-23 17:28:56 +0900

    bj15649 practice..

--- src/main/algorithm/baekjoon/Bj.java
+++ src/main/algorithm/baekjoon/Bj.java
@@ -5,12 +5,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Bj {
+	public static int[] arr;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[m];
+		
+		dfs(n, m, 0);
+	}
+	
+	public static void dfs(int N, int M, int depth) {
 		
 	}
 }
