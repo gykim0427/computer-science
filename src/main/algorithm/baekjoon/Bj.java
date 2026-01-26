1bafdc4a3271b3bef5b460c470e2c09fc5d68202
gykim <gykim0427@naver.com>
2026-01-26 17:25:42 +0900

    bj15649 dfs practice...

--- src/main/algorithm/baekjoon/Bj.java
+++ src/main/algorithm/baekjoon/Bj.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 
 public class Bj {
 	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,9 +18,20 @@ public class Bj {
 		arr = new int[m];
 		
 		dfs(n, m, 0);
+		
+		System.out.println(sb);
 	}
 	
 	public static void dfs(int N, int M, int depth) {
+		if (M != depth) {
+			for (int i : arr) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
 		
+		dfs(N, M, depth + 1);
 	}
 }
